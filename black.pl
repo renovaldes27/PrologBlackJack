f57703e7f63613031788be8e7a3aab18e82a879c
renovaldes <renovaldes@email.arizona.edu>
2017-11-30 14:16:38 -0700

    Added some predicates for dealing player/dealer cards.

--- black.pl
+++ black.pl
@@ -1,1 +1,24 @@
-getCard :- random(1,53,Random), card(R,S,V,Random), write(R),write(' of '),write(S),nl.
+:-  dynamic playerCard/4.
+:-  dynamic dealerCard/4.
+
+getCard(R,S,V,Random) :- random(1,53,Random), card(R,S,V,Random). 
+
+
+
+intro :- write('Welcome to black jack!'),nl, 
+         write('Play as long as you\'d like or until out of money.'),nl,
+         write('Are you ready to begin? '),nl.
+
+
+blackJack :- intro, readsentence(R), 
+             R = [y,e,s] -> write('place holder'); 
+                            write('Thank you for playing BlackJack.'), nl.                        
+
+                        
+dealPlayerCard :- getCard(R,S,V,Random), not(playerCard(_,_,_,Random)) -> assertz(playerCard(R,S,V,Random)) ; dealPlayerCard.
+dealDealerCard :- getCard(R,S,V,Random), not(dealerCard(_,_,_,Random)) -> assertz(dealerCard(R,S,V,Random)) ; dealDealerCard.
+
+dealCards :- dealPlayerCard, dealDealerCard.
+
+clearCards :- retract(playerCard(_,_,_,_)), retract(dealerCard(_,_,_,_)),fail.
+clearCards.
